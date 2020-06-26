bc7af5a1fdc368b7463c49e4de0cf6ec92feff76
Robert Lee <rml7du@gmail.com>
2020-06-26 10:37:48 -0600

    working state before attempting to add serialization

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,30 @@
+require 'yaml'
 
+module BasicSerialization
+    @@serializer = YAML 
 
+    def serialize
+        obj = {}
+        instance_variables.map do |var|
+            obj[var] = instance_variable_get(var)
+        end
+
+        @@serializer.dump obj
+    end
+
+    def unserialize  (string)
+        obj = @@serializer.parse(string)
+        obj.keys.each do |key|
+            instance_variable_set(key, obj[key])
+        end
+    end
+end
 
 class Board
-    attr_reader :answer, :hidden_answer
+    include BasicSerialization
+
+    attr_reader :answer, :hidden_answer, :number_incorrect, :incorrect_letters
+
     def initialize()
         @answer = create_answer()
         @hidden_answer = Array.new(@answer.length, '_')
@@ -126,6 +148,8 @@ end
 
 
 class Human
+    #include BasicSerialization
+
     def initialize()
     end
 
@@ -140,30 +164,26 @@ class Human
     end
 
     def valid_guess(guess)
-        guess.length == 1 && "#{guess}" =~ /[[:alpha:]]/
-    end
-        
+        guess.length == 1 && "#{guess}" =~ /[[:alpha:]]/ #&& "#{guess}" !=~ /[incorrect_letters]/
+    end      
 end
-
-
-
-
 
 board = Board.new()
 human = Human.new()
 
-number_incorrect = 0
-while number_incorrect < 8
-    board.print_board(number_incorrect)
+#number_incorrect = 0
+while board.number_incorrect < 8
+    board.print_board(board.number_incorrect)
     guess = human.get_guess()
-    number_incorrect = board.evaluate(guess)
+    board.evaluate(guess)
+    #number_incorrect = board.evaluate(guess)
     if board.check_winner()
-        board.print_board(number_incorrect)
+        board.print_board(board.number_incorrect)
         puts "YOU WIN!!"
         break
     end
-    if number_incorrect == 7
-        board.print_board(number_incorrect)
+    if board.number_incorrect == 7
+        board.print_board(board.number_incorrect)
         puts "GAMEOVER - the winning word was #{board.answer}"
         break
     end
